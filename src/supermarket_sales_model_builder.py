3b7f0ae47162fc2c9ca4320ea7361f56f8f799d9
Peeush Agarwal <peeush.agarwal@privacera.com>
2022-05-30 10:01:54 +0530

    Add python script for Linear regression model

---
+++ src/supermarket_sales_model_builder.py
@@ -0,0 +1,89 @@
+"""
+Usage: 
+    python supermarket_sales_model_builder.py \
+        -trp ../data/supermarket-sales/train.csv \
+        -tep ../data/supermarket-sales/test.csv \
+        -m lin_reg
+"""
+import argparse
+from cProfile import label
+from typing import Tuple
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("supermarket-sales-exp")
+
+def load_data(file_path:str) -> pd.DataFrame:
+    return pd.read_csv(file_path)
+
+def split_data(df:pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    return train_test_split(df, test_size=0.2, random_state=41)
+
+def prepare_data(df:pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+    target_col = "Item_Outlet_Sales"
+
+    X = df.drop(target_col, axis=1).values
+    y = df[target_col].values
+
+    return X, y
+
+def log_plot(y_true, y_pred, msg:str):
+    fig = plt.figure(figsize=(8, 6))
+    sns.histplot(y_true, color="blue", alpha=0.5, label="Actual")
+    sns.histplot(y_pred, color="red", alpha=0.5, label="Prediction")
+    plt.legend()
+    mlflow.log_figure(fig, artifact_file=f"plots/pred_compare_{msg}.png")
+    fig.clear(True)
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--train-path", "-trp", required=True,
+                    help="Training data file path")
+    ap.add_argument("--test-path", "-tep", required=True,
+                    help="Testing data file path")
+    ap.add_argument("--model", "-m", default="lin_reg",
+                    choices=["lin_reg"], help="Model name")
+    args = ap.parse_args()
+
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "peeush-agarwal")
+        mlflow.set_tag("model", args.model)
+        mlflow.log_param("training path", args.train_path)
+        mlflow.log_param("testing path", args.test_path)
+
+        df = load_data(args.train_path)
+        df_test = load_data(args.test_path)
+
+        df_train, df_val = split_data(df)
+        
+        X_train, y_train = prepare_data(df_train)
+        X_val, y_val = prepare_data(df_val)
+        X_test, y_test = prepare_data(df_test)
+
+        if args.model == "lin_reg":
+            lr = LinearRegression()
+            lr.fit(X_train, y_train)
+            y_train_pred = lr.predict(X_train)
+            y_val_pred = lr.predict(X_val)
+            y_test_pred = lr.predict(X_test)
+
+            rmse_calc = lambda y_t, y_p: np.sqrt(mean_squared_error(y_t, y_p))
+            mlflow.log_metric("train.rmse", rmse_calc(y_train, y_train_pred))
+            mlflow.log_metric("val.rmse", rmse_calc(y_val, y_val_pred))
+            mlflow.log_metric("test.rmse", rmse_calc(y_test, y_test_pred))
+            
+            log_plot(y_train, y_train_pred, msg="train")
+            log_plot(y_val, y_val_pred, msg="val")
+            log_plot(y_test, y_test_pred, msg="test")
+            
+            mlflow.sklearn.log_model(lr, "models_mlflow")
